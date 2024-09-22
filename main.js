86a084130ac1cd1f6abaf319c9f9e57541769763
Amir Mohammad <amirfanweb@gmail.com>
2024-09-22 21:09:32 +0330

    COMMEnt

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("dom loaded");
 });
 
-const el = document.createElement("iframe");
+// const el = document.createElement("iframe");
 
-el.src = document
-  .getElementsByTagName("iframe")[0]
-  .src.replace(/(tgWebAppPlatform=)[^&]+/, "$1android");
+// el.src = document
+//   .getElementsByTagName("iframe")[0]
+//   .src.replace(/(tgWebAppPlatform=)[^&]+/, "$1android");
 
-document.querySelector("iframe").remove();
+// document.querySelector("iframe").remove();
 
-el.height = "100%";
-el.width = "100%";
+// el.height = "100%";
+// el.width = "100%";
 
-document.querySelector("body").appendChild(el);
+// document.querySelector("body").appendChild(el);
