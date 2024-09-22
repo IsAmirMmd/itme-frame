2784fb91c9c2256a68e70f2e375f506e8127d228
Amir Mohammad <amirfanweb@gmail.com>
2024-09-22 20:16:50 +0330

    update

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("dom loaded");
 });
+
+const el = document.createElement("iframe");
+
+el.src = document
+  .getElementsByTagName("iframe")[0]
+  .src.replace(/(tgWebAppPlatform=)[^&]+/, "$1android");
+
+document.querySelector("iframe").remove();
+
+el.height = "100%";
+el.width = "100%";
+
+document.querySelector("body").appendChild(el);
